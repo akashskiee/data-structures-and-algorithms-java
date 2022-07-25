b971c685830d066da50c1cb9c92d67eea86876fd
Akash Kumar <akashpk1997@gmail.com>
2022-07-25 17:37:03 +0530

    Peak Index Array

--- algorithms/searching/binary-search/PeakIndexArray.java
+++ algorithms/searching/binary-search/PeakIndexArray.java
@@ -3,12 +3,28 @@
 public class PeakIndexArray {
     
     public static void main(String[] args) {
-        int [] arr = {0,10,5,2};
+        int [] arr = {0,1,2,1};
+        System.out.println(peakIndexInMountainArray(arr));
     }
 
 
     public static int peakIndexInMountainArray(int [] nums){
-        
-        return -1;
+        int i = 0;
+        int j = nums.length - 1;
+        int max = 0;
+
+        while(i <= j){
+            int mid = i +(j - i)/2;
+
+            if(nums[mid] < nums[mid + 1]){
+                max = mid + 1;
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+
+        }
+
+        return max;
     }
 }
