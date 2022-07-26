f4adccde221ccb75ab6a38bac75afb3ba531faec
Akash Kumar <akashpk1997@gmail.com>
2022-07-26 10:02:27 +0530

    String

---
+++ data-structures/strings/DefangIP.java
@@ -0,0 +1,21 @@
+public class DefangIP {
+    public static void main(String[] args) {
+        String address = "255.100.50.0";
+        System.out.println(defangIPaddr(address));
+    }
+
+    public static String defangIPaddr(String address) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < address.length(); i++){
+            char c = address.charAt(i);
+            if(c == '.') {
+                sb.append("[.]");
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+    
+}
---
+++ data-structures/strings/ShuffleString.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class ShuffleString {
+    public static void main(String[] args) {
+        String s = "codeleet";
+        int [] indices = {4,5,6,7,0,2,1,3};
+
+        System.out.println(restoreString(s, indices));
+    }
+
+    public static String restoreString(String s, int[] indices) {
+        char [] ch = new char[indices.length];
+        for(int i = 0; i < indices.length; i++){
+            ch[indices[i]] = s.charAt(i);
+        }
+        
+        return new String(ch);
+    }
+}
